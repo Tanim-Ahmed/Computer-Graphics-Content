9ceb289fbedc310d89fd6e45f122192b523e1385
Tanim Ahmed <tanim.ahmed.oct@gmail.com>
2022-08-10 14:23:49 +0600

    Add files via upload

--- Computer Graphics Lab task/main.cpp
+++ Computer Graphics Lab task/main.cpp
@@ -34,40 +34,7 @@ glClear (GL_COLOR_BUFFER_BIT);
 
 
 
-   glColor3f (0.0, 1.0, 0.0);
 
-	glBegin(GL_QUADS); //Begin quadrilateral coordinates
-
-	//Trapezoid
-	//glColor3f (0.0, 1.0, 1.0);
-	glVertex3f(0.05f, 0.05f, 0.0f);
-	glVertex3f(0.35f, 0.05f, 0.0f);
-	glVertex3f(0.40f, 0.35f, 0.0f);
-	glVertex3f(0.00f, 0.35f, 0.0f);
-
-	glEnd(); //End quadrilateral coordinates
-
-	glBegin(GL_TRIANGLES); //Begin triangle coordinates
-   //glColor3f (0.0, 1.0, 1.0);
-	//Pentagon
-	glVertex3f(0.5f, 0.05f, 0.0f);
-	glVertex3f(0.75f, 0.05f, 0.0f);
-	glVertex3f(0.5f, 0.35f, 0.0f);
-
-	glVertex3f(0.5f, 0.35f, 0.0f);
-	glVertex3f(0.75f, 0.05f, 0.0f);
-	glVertex3f(0.75f, 0.35f, 0.0f);
-
-	glVertex3f(0.5f, 0.35f, 0.0f);
-	glVertex3f(0.75f, 0.35f, 0.0f);
-	glVertex3f(0.63f, 0.50f, 0.0f);
-
-	//Triangle
-	glVertex3f(0.30f, 0.65f, 0.0f);
-	glVertex3f(0.60f, 0.65f, 0.0f);
-	glVertex3f(0.45, 0.85f, 0.0f);
-
-	glEnd();//End triangle coordinates
 
 
 	// x axis
@@ -88,9 +55,17 @@ glEnd();
 //points
 
 glColor3f(11,233,0);
+glPointSize(8);
 glBegin(GL_POINTS);
-glVertex3f(0.5,0.0,0.0);
-glVertex3f(0.0,0.0,0.0);
+glVertex3f(-0.5,0.0,0.0);
+
+glEnd();
+
+glBegin(GL_TRIANGLES);
+glColor3f(1.0,1.0,0.0);
+glVertex3f(0.2,0.2,0.0);
+glVertex3f(0.3,0.3,0.0);
+glVertex3f(0.4,0.2,0.0);
 glEnd();
 
 /* don't wait!
@@ -98,15 +73,7 @@ glEnd();
 */
 glFlush ();
 }
-void init (void)
-{
-/* select clearing (background) color */
-glClearColor (0.0, 0.0, 0.0, 0.0);
-/* initialize viewing values */
-glMatrixMode(GL_PROJECTION);
-glLoadIdentity();
-glOrtho(0.0, 1.0, 0.0, 1.0, -10.0, 10.0);
-}
+
 /*
 * Declare initial window size, position, and display mode
 * (single buffer and RGBA). Open window with "hello"
@@ -117,12 +84,12 @@ glOrtho(0.0, 1.0, 0.0, 1.0, -10.0, 10.0);
 int main(int argc, char** argv)
 {
 glutInit(&argc, argv);
-glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-glutInitWindowSize (600, 600);
-glutInitWindowPosition (100, 100);
-glutCreateWindow ("hello");
-init ();
+glutCreateWindow ("Test");
+glutInitWindowSize(640,500);
 glutDisplayFunc(display);
+
+
+
 glutMainLoop();
 return 0; /* ISO C requires main to return int. */
 }
